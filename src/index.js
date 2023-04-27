ca20f402869668e8e6a2009a09601c6f3e80d91d
Ishan Gupta <guptaishan54@gmail.com>
2023-04-27 07:33:04 -0700

    multi elements is jsx

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 ReactDOM.render(
-  <div>
+  <>
   <h1> hello world!!</h1>
   <p> my name is ishan</p>
   <h1>hello</h1>
-  </div>,
+  <ol>
+  <li>iron man</li>
+  <li>spider man</li>
+  <li>bat man</li>
+  </ol>
+  </>,
   document.getElementById("root"));
