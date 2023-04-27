6b12e0fcbd5c940786ae9824ca02327ddcd67857
Ishan Gupta <guptaishan54@gmail.com>
2023-04-27 04:32:10 -0700

    first try

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render([
+  <h1> hello world!!</h1>,
+  <p> my name is ishan</p>,
+  <h1>hello</h1>],
+  document.getElementById("root"));
